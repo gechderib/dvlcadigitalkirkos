b8dd2415050eb0ab147d987a68351503a6761a4b
gechderib <gechderib@gmail.com>
2024-08-19 13:44:01 +0300

    staticfiles

--- build.sh
+++ build.sh
@@ -10,3 +10,6 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+########## Run command #############
+# python -m gunicorn rent_all.asgi:application -k uvicorn.workers.UvicornWorker
