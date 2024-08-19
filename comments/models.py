6494da9e2a3925fba52552c7cd4ecd8e05feda83
gechderib <gechderib@gmail.com>
2024-08-19 14:21:43 +0300

    ticket number :

--- comments/models.py
+++ comments/models.py
@@ -9,7 +9,7 @@ class Comment(models.Model):
  content = models.TextField()
  to_user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="comments")
  level_of_satsfaction = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
- ticket = models.CharField(max_length=10, blank=False, null=False, default=123)
+ ticket = models.CharField(max_length=10, blank=False, null=False)
  created_at = models.DateTimeField(auto_now_add=True)
  updated_at = models.DateTimeField(auto_now=True)
  
