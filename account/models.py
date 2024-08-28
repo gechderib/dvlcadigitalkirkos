cbd46dcf2783216131724788168a6528fffd0140
gechderib <gechderib@gmail.com>
2024-08-28 18:11:51 +0300

    change the image thing

--- account/models.py
+++ account/models.py
@@ -1,60 +1,52 @@
-
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
-from cloudinary.models import CloudinaryField
+from cloudinary.models import CloudinaryField  # Import CloudinaryField
 
 class UserManager(BaseUserManager):
+    def create_user(self, phone_number, password=None, **extra_fields):
+        if not phone_number:
+            raise ValueError("User must have a phone number")
 
- def create_user(self,phone_number, password= None,**extra_fields):
-  if not phone_number:
-   raise ValueError("User must have a phone number")
-  
-  user = self.model(phone_number=phone_number, **extra_fields)
-  user.set_password(password)
-  user.save(using=self._db)
-  return user
- 
- def create_superuser(self, phone_number, password, **extra_fields):
+        user = self.model(phone_number=phone_number, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-  extra_fields.setdefault("is_staff", True)
-  extra_fields.setdefault("is_superuser", True)
+    def create_superuser(self, phone_number, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
 
-  if extra_fields.get("is_staff") is not True:
-   raise ValueError("Super user must have staff = True")
-  if extra_fields.get("is_superuser") is not True:
-   raise ValueError("Super user must have is_superuser = True")
-  
-  return self.create_user(phone_number, password, **extra_fields)
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Super user must have staff = True")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Super user must have is_superuser = True")
 
+        return self.create_user(phone_number, password, **extra_fields)
 
 ROLE_CHOICES = (
- ('admin', 'Admin'),
- ('staff', 'Staff'),
- ('user', 'User'),
- ('user1', 'User1'),
- ('user2', 'User2'),
- ('user3', 'User3'),
- ('user4', 'User4'),
+    ('admin', 'Admin'),
+    ('staff', 'Staff'),
+    ('user', 'User'),
+    ('user1', 'User1'),
+    ('user2', 'User2'),
+    ('user3', 'User3'),
+    ('user4', 'User4'),
 )
 
-
 class CustomUser(AbstractUser):
+    first_name = models.CharField(verbose_name="First Name", max_length=255)
+    last_name = models.CharField(verbose_name="Last Name", max_length=255)
+    phone_number = models.CharField(verbose_name="Phone Number", max_length=15, unique=True, blank=False, null=False)
+    password = models.CharField(verbose_name="Password", max_length=255, blank=False, null=False)
+    role = models.CharField(verbose_name="Role", max_length=20, choices=ROLE_CHOICES, default='staff')
+    profile_pic = CloudinaryField('image', blank=True, null=True)
 
- first_name = models.CharField(verbose_name="First Name", max_length=255)
- last_name = models.CharField(verbose_name="Last Name", max_length=255)
- phone_number = models.CharField(verbose_name="Phone Number", max_length=15, unique=True, blank=False, null=False)
- password = models.CharField(verbose_name="Password", max_length=255, blank=False, null=False)
- role = models.CharField(verbose_name="Role", max_length=20, choices=ROLE_CHOICES, default='staff')
- profile_pic = CloudinaryField("profile_pic", default=None, blank=False)
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    username = None
+    email = None
+    objects = UserManager()
 
- def __str__(self):
-  return f"{self.first_name} {self.last_name}"
- 
- username = None
- email = None
- objects = UserManager()
- 
- USERNAME_FIELD = 'phone_number'
- REQUIRED_FIELDS = ['first_name', "last_name", "profile_pic", "role"]
-
- 
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'role']
