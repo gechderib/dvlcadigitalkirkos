a2aa37fc43b1b01bb6f6ef42e39d4867c03d5e28
gechderib <gechderib@gmail.com>
2024-08-29 18:12:37 +0300

    adding srvice availability

--- fileprocess/admin.py
+++ fileprocess/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import FileProcess
+from .models import ServiceAvailability
 # Register your models here.
 
 
 admin.site.register(FileProcess)
+admin.site.register(ServiceAvailability)
