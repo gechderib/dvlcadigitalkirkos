3efb956d60ef86eb4b288e7a881ee29fef1fe0b0
gechderib <gechderib@gmail.com>
2024-08-29 15:04:51 +0300

    serailizer fix for ticker

--- news/serializers.py
+++ news/serializers.py
@@ -36,14 +36,16 @@ class TicketAnouncementSerializer(serializers.ModelSerializer):
     def validate(self, data):
         current_ticket_number = data['current_ticket_number']
         last_ticket_number = data['last_ticket_number']
-        
+
         # Ensure current_ticket_number is less than last_ticket_number
         if int(current_ticket_number) >= int(last_ticket_number):
             raise ValidationError("The current ticket number must be less than the last ticket number.")
         
-        # Get the most recent last_ticket_number from the database
-        last_ticket = TicketAnnouncement.objects.order_by('-updated_at').first()
-        if last_ticket and int(last_ticket_number) <= int(last_ticket.last_ticket_number):
-            raise ValidationError("The last ticket number must be greater than the previously added last ticket number.")
+        # Get the most recent ticket announcement within the last 24 hours
+        last_24_hours = timezone.now() - timedelta(hours=24)
+        recent_ticket = TicketAnnouncement.objects.filter(updated_at__gte=last_24_hours).order_by('-updated_at').first()
+
+        if recent_ticket and int(last_ticket_number) <= int(recent_ticket.last_ticket_number):
+            raise ValidationError("The last ticket number must be greater than the previously added last ticket number within the last 24 hours.")
         
         return data
